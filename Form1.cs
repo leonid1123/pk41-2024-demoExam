899ecbd35a1467faa19314a2edc1ff5984d0726a
leonid1123 <47742726+leonid1123@users.noreply.github.com>
2024-10-25 11:47:36 +0300

    new

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,12 @@
 using MySqlConnector;
+using System.Diagnostics;
 
 namespace pk41_demoExam
 {
     public partial class Form1 : Form
     {
+        List<int> idPartnersList = new List<int>();
+        List<int> discounts = new List<int>();
         public Form1()
         {
             InitializeComponent();
@@ -13,9 +16,55 @@ namespace pk41_demoExam
         {
             using var connection = new MySqlConnection("Server=localhost;User ID=pk41;Password=123456;Database=partners");
             connection.Open();
+            var command = new MySqlCommand();
+            command.Connection = connection;
 
-            using var command = new MySqlCommand("SELECT Name, Country, Website, Founded FROM partners LIMIT 10;", connection);
-            using var reader = command.ExecuteReader();
+            //получить все id партнеров из таблицы sales
+
+            command.CommandText = "SELECT DISTINCT id_partner FROM sales";
+            command.ExecuteNonQuery();
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                idPartnersList.Add(reader.GetInt32(0));
+            }
+
+            //посчитать все продажи И скидку
+
+            foreach (var item in idPartnersList)
+            {
+                reader.Close();
+                command.CommandText = "SELECT SUM(quantity) FROM sales WHERE id_partner = @p";
+                command.Parameters.Clear();
+                command.Parameters.AddWithValue("p", item);
+                command.ExecuteNonQuery();
+
+                reader = command.ExecuteReader();
+                while (reader.Read())//расчет скидки
+                {
+                    var tmp = reader.GetInt32(0);
+                    if (tmp < 10000)
+                    {
+                        discounts.Add(0);
+                    } else if(tmp > 10000 & tmp < 50000)
+                    {
+                        discounts.Add(5);
+                    } else if (tmp > 50000 & tmp < 300000)
+                    {
+                        discounts.Add(10);
+                    } else
+                    {
+                        discounts.Add(15);
+                    }
+                }
+            }
+
+            //получить список всех партнеров
+
+            reader.Close();
+            command.CommandText = "SELECT Name, Country, Website, Founded FROM partners LIMIT 10;";
+            command.ExecuteNonQuery();
+            reader = command.ExecuteReader();
             int i = 0;
             while (reader.Read())
             {
@@ -50,18 +99,19 @@ namespace pk41_demoExam
                 foundedLabel.Name = "Founded" + i.ToString();
                 foundedLabel.Text = ansFounded.ToString();
 
+                Label discountLabel = new Label();
+                discountLabel.Location = new System.Drawing.Point(200, 0 + i * 80);
+                discountLabel.Parent = this;
+                discountLabel.Name = "discount" + i.ToString();
+                discountLabel.Text = "Скидка:"; //вывести скидку сюда
+
                 this.Controls.Add(nameLabel);
                 this.Controls.Add(countryLabel);
                 this.Controls.Add(webLabel);
                 this.Controls.Add(foundedLabel);
 
                 i++;
-            }
-
-
-
-
-
+            }           
         }
     }
 }
